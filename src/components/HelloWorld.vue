27e255a2b6b47c62a1d81f2b8be4b7d544c50b06
chenhemei <1914163984@qq.com>
2018-12-27 17:50:28 +0800

    动态生成from

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <!-- <p>1234</p> -->
+    <v-from :inintData='inintForm'></v-from>
+    <!-- <v-from></v-from> -->
+  </div>
+</template>
+<script>
+import vfrom from './from'
+import { jsajx } from '../url/jsajx'
+export default {
+  components: {
+    'v-from': vfrom
+  },
+  data () {
+    var phoneyz = (rule, value, callback) => {
+      if (!/^1[34578]\d{9}$/.test(value)) {
+        callback(new Error('手机号码有误，请重填'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      inintForm: {
+        fromData: [
+          {
+            type: 'input',
+            paramter: 'name',
+            name: '姓名：',
+            value: '',
+            placrholder: '请输入...',
+            rules: [
+              { required: true, message: '此处不能为空', trigger: 'blur' }
+            ]
+          },
+          {
+            type: 'input',
+            paramter: 'name',
+            name: '电话：',
+            value: '',
+            placrholder: '请输入...',
+            rules: [
+              { required: true, message: '此处不能为空', trigger: 'blur' },
+              {
+                validator: phoneyz,
+                message: '手机号码有误，请重填',
+                trigger: 'blur'
+              }
+            ]
+          },
+          {
+            type: 'upImg',
+            paramter: 'name',
+            name: '头像：',
+            value: '',
+            file: '',
+            rules: [
+              { required: true, message: '此处不能为空', trigger: 'blur' }
+            ]
+          },
+          {
+            type: 'input',
+            paramter: 'name',
+            name: '密码：',
+            value: '',
+            placrholder: '请输入...',
+            rules: [
+              { required: true, message: '此处不能为空', trigger: 'blur' }
+            ]
+          },
+          {
+            type: 'select',
+            multiple: true,
+            paramter: 'name',
+            name: '角色：',
+            value: [],
+            option: [],
+            rules: [
+              {
+                type: 'array',
+                required: true,
+                message: '此处不能为空',
+                trigger: 'change'
+              }
+            ]
+          },
+          {
+            type: 'treeSelect',
+            paramter: 'name',
+            name: '部门：',
+            value: '',
+            option: [],
+            rules: [
+              { required: true, message: '此处不能为空', trigger: 'blur' }
+            ]
+          },
+          {
+            type: 'slideButton',
+            parameter: 'name',
+            name: '是否启用',
+            value: true
+          }
+        ]
+      }
+    }
+  },
+  mounted () {
+    // jsajx('json', 'get', '/admin/role/getrolekv.json', '', res => {
+    //   res = JSON.parse(res)
+    //   res.data.forEach(element => {
+    //     element.value = element.id
+    //   })
+    //   this.inintForm.fromData[4].option = res.data || []
+    // })
+  }
+}
+</script>
